975c5e0f5c3a69f05f7ec8a18c54da16e828c578
Adnaan khan <139698328+Khan012003@users.noreply.github.com>
2025-03-23 13:35:13 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.https://mern-app-backend-yndp.onrender.com,
   withCredentials: true
 });
 
